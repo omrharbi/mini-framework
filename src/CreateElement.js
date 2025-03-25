5a50f2351877061179c5033d4ea73365be53ac41
saljaoui <soufian2022ff@gmail.com>
2025-03-25 18:42:32 +0000

    add store and handel ensirt todos list

--- src/CreateElement.js
+++ src/CreateElement.js
@@ -1,45 +1,91 @@
-function createElement(type, props, ...children) {
+// function createElement(type, props, ...children) {
+//     return {
+//         type,
+//         props: {
+//             ...props,
+//             children: children.map(child =>
+//                 typeof child === "object"
+//                     ? child
+//                     : createTextElement(child)
+//             ),
+//         },
+//     }
+// }
+
+// function createTextElement(text) {
+//     return {
+//         type: "TEXT_ELEMENT",
+//         props: {
+//             nodeValue: text,
+//             children: [],
+//         },
+//     }
+// }
+
+// function render(element, container) {
+//     const dom =
+//         element.type == "TEXT_ELEMENT"
+//             ? document.createTextNode("")
+//             : document.createElement(element.type)
+
+//     const isProperty = key => key !== "children"
+//     Object.keys(element.props)
+//         .filter(isProperty)
+//         .forEach(name => {
+//             if (name.startsWith('on') && typeof element.props[name] === 'function') {
+//                 const event = name.slice(2).toLowerCase();
+//                 dom.addEventListener(event, element.props[name]);
+//             } else {
+//                 dom[name] = element.props[name];
+//             }
+//         })
+
+//     element.props.children.forEach(child =>
+//         render(child, dom)
+//     )
+
+//     container.appendChild(dom)
+// }
+
+// export const Didact = {
+//     createElement,
+//     render,
+// }
+
+
+
+
+export function createElement(tag, attrs = {}, ...children) {
     return {
-        type,
-        props: {
-            ...props,
-            children: children.map(child =>
-                typeof child === "object"
-                    ? child
-                    : createTextElement(child)
-            ),
-        },
-    }
+        tag,
+        attrs,
+        children: children.flat()
+    };
 }
 
-function createTextElement(text) {
-    return {
-        type: "TEXT_ELEMENT",
-        props: {
-            nodeValue: text,
-            children: [],
-        },
+function render(vnode, parent) {
+    const mount = parent ? (el) => { parent.innerHTML = ''; parent.appendChild(el) } : (el) => el;
+    
+    if (typeof vnode === 'string') {
+        return mount(document.createTextNode(vnode));
     }
-}
 
-function render(element, container) {
-    const dom =
-        element.type == "TEXT_ELEMENT"
-            ? document.createTextNode("")
-            : document.createElement(element.type)
+    const el = document.createElement(vnode.tag);
 
-    const isProperty = key => key !== "children"
-    Object.keys(element.props)
-        .filter(isProperty)
-        .forEach(name => {
-            dom[name] = element.props[name]
-        })
+    for (const [key, value] of Object.entries(vnode.attrs || {})) {
+        if (key.startsWith('on') && typeof value === 'function') {
+            const event = key.slice(2).toLowerCase();
+            el.addEventListener(event, value);
+        } else {
+            el.setAttribute(key, value);
+        }
+    }
 
-    element.props.children.forEach(child =>
-        render(child, dom)
-    )
+    for (const child of vnode.children) {
+        el.appendChild(render(child));
+    }
 
-    container.appendChild(dom)
+    return mount(el);
 }
 
 export const Didact = {
--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
-import {Didact} from './CreateElement.js'
-const element =
-    Didact.createElement('div', { className: 'todo-container' },
-        // Didact.createElement('button', { className: 'add-button' },
+import { Didact } from './CreateElement.js'
+import { createStore } from './store.js';
+
+const initialState = {
+    todos: [],
+    filter: 'all'
+};
+
+const store = createStore(initialState);
+
+function App() {
+
+    const { todos, filter } = store.getState();
+    console.log(filter); // catkon 'all' fe bdya mn b3d o nbdloha
+
+    const filteredTodos = filterTodos(todos, filter)
+
+    return Didact.createElement('div', { class: 'todo-container' },
+        // Didact.createElement('button', { class: 'add-button' },
         //     Didact.createElement('svg', { xmlns: "http://www.w3.org/2000/svg", width: "28", height: "28", viewBox: "0 0 24 24" },
         //         Didact.createElement('g', { fill: "none", fillRule: "evenodd", stroke: "#fff", strokeLinecap: "round", strokeLinejoin: "round", strokeWidth: "2", transform: "translate(2 2)" },
         //             Didact.createElement('line', { x1: "10", x2: "10", y1: "6.327", y2: "13.654" }),
@@ -10,33 +25,44 @@ const element =
         //         )
         //     )
         // ),
-        
+        Didact.createElement('div', {class:'todo-header'},
         Didact.createElement('input', { 
             type: 'text', 
-            className: 'todo-input', 
-            placeholder: 'Add a new task...', 
-            onInput: (e) => {
-                console.log(e.target.value);
+            class: 'todo-input',
+            placeholder: 'What needs to be done?',
+            onKeyup: (e) => {
+                if (e.key === 'Enter' && e.target.value.trim()) {
+                    store.dispatch({
+                        type: 'ADD_TODO',
+                        payload: {
+                            id: Date.now(),
+                            text: e.target.value.trim(),
+                            completed: false
+                        }
+                    });
+                    store.subscribe(update);
+                    e.target.value = '';
+                }
             }
         }),
-        Didact.createElement('div', { className: 'tab-container' },
-            Didact.createElement('button', { className: 'tab' }, 'Active'),
-            Didact.createElement('button', { className: 'tab' }, 'Completed'),
-            Didact.createElement('button', { className: 'tab' }, 'All todos')
+    ),
+
+        Didact.createElement('div', { class: 'tab-container' },
+            Didact.createElement('button', { class: 'tab' }, 'Active'),
+            Didact.createElement('button', { class: 'tab' }, 'Completed'),
+            Didact.createElement('button', { class: 'tab' }, 'All todos')
         ),
-        Didact.createElement('div', { className: 'todo-list' },
-            Didact.createElement('div', { className: 'todo-item completed' },
-                Didact.createElement('div', { className: 'todo-checkbox checked' }),
-                Didact.createElement('div', { className: 'todo-text', style: 'text-decoration: line-through;' }, "Don't worry"),
-                Didact.createElement('button', { className: 'delete-button' }, '×')
-            ),
-            Didact.createElement('div', { className: 'todo-item' },
-                Didact.createElement('div', { className: 'todo-checkbox' }),
-                Didact.createElement('div', { className: 'todo-text' }, 'Be happy'),
-                Didact.createElement('button', { className: 'delete-button' }, '×')
-            )
-        ),
-        // Didact.createElement('button', { className: 'trash-button' },
+
+        TodoList(filteredTodos),
+         
+        Didact.createElement('div', { class: 'todo-item' },
+                Didact.createElement('div', { class: 'todo-checkbox' }),
+                Didact.createElement('div', { class: 'todo-text' }, 'Be happy'),
+                Didact.createElement('button', { class: 'delete-button' }, '×')
+        )
+        )
+
+        // Didact.createElement('button', { class: 'trash-button' },
         //     Didact.createElement('svg', { xmlns: "http://www.w3.org/2000/svg", width: "28", height: "28", fill: "#fff", viewBox: "0 0 24 24" },
         //         Didact.createElement('g', null,
         //             Didact.createElement('path', { d: "M18 4h-1V3a3 3 0 0 0-3-3h-4a3 3 0 0 0-3 3v1H6a3 3 0 0 0-3 3v2a1 1 0 0 0 1 1h.069l.8 11.214A3.012 3.012 0 0 0 7.862 24h8.276a3.012 3.012 0 0 0 2.992-2.786L19.931 10H20a1 1 0 0 0 1-1V7a3 3 0 0 0-3-3ZM9 3a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1H9ZM5 7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v1H5Zm12.136 14.071a1 1 0 0 1-1 .929H7.862a1 1 0 0 1-1-.929L6.074 10h11.852Z" }),
@@ -44,6 +70,34 @@ const element =
         //         )
         //     )
         // )
-    );
+        
+}
 
-Didact.render(element, document.body)
+function TodoList(todos) {
+return Didact.createElement('div', { class: 'todo-list' },
+    ...todos.map(todo => 
+        Didact.createElement('div', { class: `todo-item ${todo.completed ? 'completed' : ''}` },
+        Didact.createElement('div', { class: `todo-checkbox ${todo.completed ? 'checked' : ''}` }),
+        Didact.createElement('div', { class: 'todo-text', style: `${todo.completed ? 'text-decoration: line-through;' : '' }` }, todo.text),
+        Didact.createElement('button', { 
+            class: 'delete-button'
+            , onClick: () => store.dispatch({ type: 'DELETE_TODO', payload: todo.id })
+        }, '×')
+    )
+),
+)
+}
+
+function filterTodos(todos, filter) {
+    switch (filter) {
+        case 'active': return todos.filter(t => !t.completed);
+        case 'completed': return todos.filter(t => t.completed);
+        default: return todos;
+    }
+}
+
+function update() {
+    Didact.render(App(), document.getElementById('root'));
+}
+store.subscribe(update);
+update();
