44230685e5a46d512ebca59f22b280b516a5ae7f
omrharbi <rharbu.omar@gmail.com>
2025-04-08 17:22:26 +0100

    done

---
+++ mini-framwork/app.js
@@ -0,0 +1,19 @@
+function App() {
+  const [state, setState] = useState("hello");
+  return jsx(
+    "h1",
+    { className: "new" },
+    "Simple Test",
+    jsx(
+      "button",
+      {
+        onclick: () => {
+          setState("you are here");
+        },
+      },
+      "Click Here"
+    ),
+    jsx("p", null, state)
+  );
+}
+render();
